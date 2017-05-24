0caf8d55a6e518e9b6313e49b544a506e9828221
user <user@userdeiMac.local>
2017-05-24 12:01:09 -0700

    mod

---
+++ reconstruction/src/main/java/com/hhyg/TyClosing/presenter/BindPrivilegePresenter.java
@@ -0,0 +1,55 @@
+package com.hhyg.TyClosing.presenter;
+
+import android.os.Message;
+
+import com.alibaba.fastjson.JSONException;
+import com.alibaba.fastjson.JSONObject;
+import com.hhyg.TyClosing.allShop.handler.SimpleHandler;
+import com.hhyg.TyClosing.config.Constants;
+import com.hhyg.TyClosing.global.MyApplication;
+import com.hhyg.TyClosing.global.NetCallBackHandlerException;
+import com.hhyg.TyClosing.global.ProcMsgHelper;
+import com.hhyg.TyClosing.mgr.ClosingRefInfoMgr;
+import com.hhyg.TyClosing.view.BindPrivilegeView;
+
+import java.io.IOException;
+
+/**
+ * Created by user on 2017/5/24.
+ */
+
+public class BindPrivilegePresenter extends BasePresenter<BindPrivilegeView> {
+
+    private final String BIND_URL = Constants.getIndexUrl() + "?r=privilege/bound";
+    private final BindPrivilegeExceptionHandler mExceptionHandler = new BindPrivilegeExceptionHandler();
+    private final BindPrivilegeProc mProc = new BindPrivilegeProc();
+
+    public void bindPrivilegeCode(String code){
+        mHttpRequester.post(BIND_URL,makeBindParam(code),new NetCallBackHandlerException(mExceptionHandler,mProc));
+    }
+
+    private String makeBindParam(String code){
+        JSONObject param = new JSONObject();
+        param.put("op", "privilege_bound");
+        param.put("imei", MyApplication.GetInstance().getAndroidId());
+        param.put("shopid", ClosingRefInfoMgr.getInstance().getShopId());
+        param.put("channel", ClosingRefInfoMgr.getInstance().getChannelId());
+        param.put("codes",code);
+        param.put("platformId",3);
+        return param.toString();
+    }
+
+    class BindPrivilegeExceptionHandler extends SimpleHandler{
+        @Override
+        public void handleMessage(Message msg) {
+            super.handleMessage(msg);
+        }
+    }
+
+    class BindPrivilegeProc implements ProcMsgHelper{
+        @Override
+        public void ProcMsg(String msgBody) throws JSONException, IOException {
+        }
+    }
+
+}
